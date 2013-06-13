ca5d66457fe13b9f519b4210f43c339cd36ed861
Brian Enigma <brian@netninja.com>
2013-06-13 14:01:38 -0700

    Don't butt-ize form fields used by the Trac issue tracker

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,3 +1,12 @@
+ID_IGNORE_LIST = [
+    // Don't traverse into the input and textarea fields of the Trac issue 
+    // tracking system.  Unintentional, unexpected, and hilarious results
+    // arise from this script editing those fields when editing a ticket.
+    'comment',
+    'field-summary',
+    'field-description'
+];
+
 walk(document.body);
 
 function walk(node) 
@@ -12,6 +21,11 @@ function walk(node)
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
+            if (null != node.getAttribute('id'))
+            {
+                if (ID_IGNORE_LIST.indexOf(node.getAttribute('id')) != -1)
+                    break;
+            }
 			child = node.firstChild;
 			while ( child ) 
 			{
